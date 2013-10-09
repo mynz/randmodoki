727d1ff61707e8f9e6b572dad44d812cb5654e24
Atsushi_Morimoto <rollingteapot@gmail.com>
2013-10-10 02:01:08 +0900

    build ok

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PATH: ${.CURDIR}
 
-KMOD= zeromodoki
-SRCS= zeromodoki.c device_if.h bus_if.h
+KMOD= randmodoki
+SRCS= randmodoki.c device_if.h bus_if.h
 
 .include <bsd.kmod.mk>
 
--- randmodoki.c
+++ randmodoki.c
@@ -16,6 +16,10 @@ __FBSDID("$FreeBSD: release/9.1.0/sys/dev/null/null.c 230320 2012-01-18 21:54:34
 #include <machine/bus.h>
 #include <machine/vmparam.h>
 
+
+#include <sys/selinfo.h>
+#include "randomdev.h"
+
 /* For use with destroy_dev(9). */
 static struct cdev *randmodoki_dev;
 
@@ -65,8 +69,30 @@ randmodoki_ioctl(struct cdev *dev __unused, u_long cmd, caddr_t data __unused,
 
 /* ARGSUSED */
 static int
-randmodoki_read(struct cdev *dev __unused, struct uio *uio, int flags __unused)
+randmodoki_read(struct cdev *dev __unused, struct uio *uio, int flag)
 {
+#if 1
+	int c, error = 0;
+	void *random_buf;
+
+	/* Blocking logic */
+	if (!random_systat.seeded)
+		error = (*random_systat.block)(flag);
+
+	/* The actual read */
+	if (!error) {
+
+		random_buf = (void *)malloc(PAGE_SIZE, M_TEMP, M_WAITOK);
+
+		while (uio->uio_resid > 0 && !error) {
+			c = MIN(uio->uio_resid, PAGE_SIZE);
+			c = (*random_systat.read)(random_buf, c);
+			error = uiomove(random_buf, c, uio);
+		}
+		free(random_buf, M_TEMP);
+	}
+	return (error);
+#else
 	void *zbuf;
 	ssize_t len;
 	int error = 0;
@@ -82,6 +108,7 @@ randmodoki_read(struct cdev *dev __unused, struct uio *uio, int flags __unused)
 	}
 
 	return (error);
+#endif
 }
 
 /* ARGSUSED */
